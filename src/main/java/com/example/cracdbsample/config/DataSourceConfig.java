aaf2d4d05032c328e16e19a9e1a3902bfd187593
Olga MaciaszekSharma <omaciaszeksh@vmware.com>
2023-09-27 21:41:08 +0200

    Fix DataSourceConfig.

--- src/main/java/com/example/cracdbsample/config/DataSourceConfig.java
+++ src/main/java/com/example/cracdbsample/config/DataSourceConfig.java
@@ -2,6 +2,8 @@ package com.example.cracdbsample.config;
 
 import javax.sql.DataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -21,11 +23,14 @@ public class DataSourceConfig {
 	@Bean
 	@RefreshScope
 	public DataSource dataSource() {
-		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-		dataSourceBuilder.driverClassName("org.postgresql.Driver");
-		dataSourceBuilder.url("jdbc:postgresql://localhost:5432/database");
-		dataSourceBuilder.username("user");
-		dataSourceBuilder.password(password);
-		return dataSourceBuilder.build();
+		DataSourceBuilder<HikariDataSource> dataSourceBuilder = DataSourceBuilder.create()
+				.type(HikariDataSource.class)
+				.driverClassName("org.postgresql.Driver")
+				.url("jdbc:postgresql://localhost:5432/database")
+				.username("user")
+				.password(password);
+		HikariDataSource dataSource = dataSourceBuilder.build();
+		dataSource.setAllowPoolSuspension(true);
+		return dataSource;
 	}
 }
